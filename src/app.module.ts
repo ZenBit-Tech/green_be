12167ebb493ee3c5d0b5ba344945736331f7bda2
StanislavGerasimov <g.stas04@gmail.com>
2025-11-11 23:11:18 +0200

    fix: getting back

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { envValidationSchema } from '@/config/env.validation';
         password: config.get<string>('DB_PASS'),
         database: config.get<string>('DB_NAME'),
         autoLoadEntities: true,
-        synchronize: false,
+        synchronize: true,
       }),
     }),
 
